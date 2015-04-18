a094b61f54df1ba320ee4f291270400453a8bc92
Denis <dingo_bastard@hotmail.com>
2015-04-18 12:09:17 +0200

    Added image and changed a code a bit

--- example/init.js
+++ example/init.js
@@ -6,17 +6,13 @@ jQuery(document).ready(function($) {
         var items = $slider.data("items");
         var easing = $slider.data("easing");
         var duration = $slider.data("duration");
-        var outer_width = $(window).outerWidth();
         var $single_slide = $slider.find('.single_slide');
         var slider_height = $single_slide.css('height', $slider.data('height')+'px');
-        var slide_width = $single_slide.width();
-
-
-        $single_slide.eq(0).addClass('active');
         var left_offset = ($(window).width()-1260)/2;
 
         $slider.css({'width' : $single_slide.length*1260+'px', 'left':- 1260+left_offset + 'px'});
 
+        $single_slide.eq(0).addClass('active');
         $('.single_slide:last-child').prependTo($slider);
 
         var $prev = $('.active').prev();
@@ -24,28 +20,25 @@ jQuery(document).ready(function($) {
 
         function moveLeft() {
             var $a = $('.active');
-            $a.removeClass('active').eq(0).addClass('active');
-
+            $a.removeClass('active').prev().addClass('active');
             $slider.animate({
                 left: parseInt($slider.css('left'), 10) + $single_slide.outerWidth(true),
                 easing: easing,
                 step: items,
             }, duration, function () {
-                $('.single_slide:last-child').prependTo($slider);
+                $('.single_slide:first').before($('single_slide:last'));
             });
         }
    
        function moveRight() {
             var $a = $('.active');
-
-            $a.removeClass('active').eq(0).addClass('active');
-
+            $a.removeClass('active').next().addClass('active');
             $slider.animate({
                 left: parseInt($slider.css('left'), 10) - $single_slide.outerWidth(true),
                 easing: easing,
                 step: items,
             }, duration, function () {
-                $('.single_slide:first-child').appendTo($slider);
+                $('.single_slide:last').before($('single_slide:first'));
             });
         }
 
